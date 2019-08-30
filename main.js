a4496d6ade0f8671196d785d6393b43ce9ab900e
jpark799 <jason.j.park79@gmail.com>
2019-08-30 16:15:06 +0900

    updated conditional statement to not always read truthy

--- main.js
+++ main.js
@@ -29,9 +29,8 @@ printBoard = () => {
   moveCounter()
 }
 
-// WIP: This function is always getting called. Look into conditional statement
 checkWinner = () => {
-  if (towers = [[], [3, 2, 1],[]] || [[], [], [3, 2, 1]]) {
+  if ((towers === [[], [3, 2, 1],[]]) || (towers === [[], [], [3, 2, 1]])) {
     console.log('Congratulations, you won! It took you ', count, ' total moves.')
     resetBoard()
   }
@@ -47,35 +46,6 @@ resetBoard = () => {
   towers = [[3, 2, 1,], [], []]
   count = 0
 }
-
-//   var moveTowers = function(start,end) {
-//     var startPeg = start - 1; // so user doesn't have to ask for 0 when they mean 1
-//     var endPeg = end - 1;     // so user doesn't have to ask for 0 when they mean 1
-//     var emptyEndPeg = towers[endPeg].length === 0; // checks if peg is empty. if it's empty, merging is easier.
-//     var notEmptyStartPeg = towers[startPeg].length != 0; // you don't want to waste a move moving an empty start peg into a peg that has value.
-//     var lastItemInStartArray = towers[startPeg][towers[startPeg].length - 1]; // Last Item In Start Array
-//     var lastItemInEndArray = towers[endPeg][towers[endPeg].length-1]; // Last Item in End Array
-//     if (lastItemInStartArray < lastItemInEndArray || emptyEndPeg && notEmptyStartPeg) {
-//       towers[endPeg].push(lastItemInStartArray);
-//       towers[startPeg].pop();
-//       count++;
-//     } else {
-//       console.log("Move is not legal.");
-//     }
-
-//     console.log(towers);
-//     console.log('Move Count: ' + count);
-//     checkWinner();
-//   }
-
-//     // Does not run properly
-//     var checkWinner = function() {
-//       if (towers === [[],[3,2,1],[]] || towers === [[],[],[3,2,1]]) {
-//         console.log("You Win!!")
-//       };
-//       };
-
-
 
 // moveTowers(1,2);
 // moveTowers(1,3);
