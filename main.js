3c1366307dbc0f7d0dbbaf1bb18279c883cd3824
jpark799 <jason.j.park79@gmail.com>
2019-09-09 14:53:43 +0900

    commented out winning combination

--- main.js
+++ main.js
@@ -51,10 +51,10 @@ resetBoard = () => {
 
 
 // uncomment for winning combination
-moveTowers(1,2);
-moveTowers(1,3);
-moveTowers(2,3);
-moveTowers(1,2);
-moveTowers(3,1);
-moveTowers(3,2);
-moveTowers(1,2);
+// moveTowers(1,2);
+// moveTowers(1,3);
+// moveTowers(2,3);
+// moveTowers(1,2);
+// moveTowers(3,1);
+// moveTowers(3,2);
+// moveTowers(1,2);
